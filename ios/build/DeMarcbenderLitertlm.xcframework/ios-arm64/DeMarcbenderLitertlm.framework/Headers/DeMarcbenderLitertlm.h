aa081dbf3190b8336893e4e2875e095f179acf1c
Marc Bender <marcbender@MacBook-Pro.local>
2026-05-07 16:33:45 +0200

    Update project.pbxproj and manifest for build

---
+++ ios/build/DeMarcbenderLitertlm.xcframework/ios-arm64/DeMarcbenderLitertlm.framework/Headers/DeMarcbenderLitertlm.h
@@ -0,0 +1,17 @@
+//
+//  DeMarcbenderLitertlm.h
+//  TitaniumLiteRTLM
+//
+//  Created by Marc Bender
+//  Copyright (c) 2026 by Your Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeMarcbenderLitertlm.
+FOUNDATION_EXPORT double DeMarcbenderLitertlmVersionNumber;
+
+//! Project version string for DeMarcbenderLitertlm.
+FOUNDATION_EXPORT const unsigned char DeMarcbenderLitertlmVersionString[];
+
+#import "DeMarcbenderLitertlmModuleAssets.h"
